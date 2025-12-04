9ca20786d516de1a3f794ce93362dad92b8bc739
Artur Khusainov <xusainow.artur2018@yandex.ru>
2025-12-05 02:16:03 +0700

    cartpole: random search

---
+++ cartpole/cartpole_random_search.py
@@ -0,0 +1,21 @@
+import gym
+import numpy as np
+from env.env_utils import evaluate_policy
+
+def random_policy(theta, state):
+    return 0 if np.dot(theta, state) < 0 else 1
+
+def train_random_search(episodes=2000):
+    env = gym.make("CartPole-v1")
+    best_reward = 0
+    best_theta = np.random.randn(4)
+
+    for episode in range(episodes):
+        theta = np.random.randn(4)
+        reward = evaluate_policy(env, lambda s: random_policy(theta, s), episodes=1)
+
+        if reward > best_reward:
+            best_reward = reward
+            best_theta = theta
+
+    return best_theta, best_reward
