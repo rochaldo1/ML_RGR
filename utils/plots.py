fc2ddbef02c611e6be961f5065efee7053aa133b
Artur Khusainov <xusainow.artur2018@yandex.ru>
2025-12-05 02:18:27 +0700

    plots & requirements

---
+++ utils/plots.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot_rewards(rewards, title):
+    plt.figure()
+    plt.plot(rewards)
+    plt.title(title)
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+    plt.grid()
+    plt.show()
